b39ead632e160ca54f7c93a354d104e96ac15538
YaYanOk <69679229+YaYanOk@users.noreply.github.com>
2020-08-19 13:08:44 +0300

    name

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
  let input = document.getElementById("input");
- let num = document.querySelectorAll(".number");
- let result = document.getElementById("result");
- let c = document.getElementById("c");
- let del = document.getElementById("delete");
+let num = document.querySelectorAll(".number");
+let result = document.getElementById("result");
+let c = document.getElementById("c");
+let del = document.getElementById("delete");
 
- [].forEach.call( num, function(el) {
-        el.onclick = function(e) {
-            input.innerText += el.innerText
-        };
-    });
- result.onclick = function(e) {
- 	if (input.innerText == "0/0") {
- 			result = "dibil"
- 			input.innerText = result;
- 	}
- 	else
- 	(result = eval(input.innerText));
- 	{
- 		input.innerText = result;
- };
- };
-c.onclick = function(e){
-	c = "";
-	input.innerText = c;
-};
-del.onclick = function(e){
-	input.slice(0, -1);
+[].forEach.call(num, function (el) {
+  el.onclick = function (e) {
+  input.classList.remove("animated", "bounce");
+  input.innerText += el.innerText;
+    input.classList.add("animated", "bounce");
+  };
+});
+result.onclick = function (e) {
+  if (input.innerText == "0/0") {
+    result = "dibil";
+    input.innerText = result;
+  } else {
+    result = eval(input.innerText);
+    input.innerText = result;
+  }
 };
 
+c.onclick = function (e) {
+  c = "";
+  input.innerText = c;
+};
 
+del.onclick = function (e) {
+  input.innerText = input.innerText.slice(0, -1);
+};
